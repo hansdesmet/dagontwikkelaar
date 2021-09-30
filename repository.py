7c07f9b4a711a764b18171dee5b259c22f1da32b
Hans Desmet <hans.desmet@vdab.be>
2021-09-30 11:54:34 +0200

    extras voor heroku

--- repository.py
+++ repository.py
@@ -1,6 +1,14 @@
 from sqlalchemy import create_engine
+from os import environ
 
-engine = create_engine("postgresql://petlover:petlover@localhost/catdog")
+if 'DATABASE_URL' in environ:
+    # web applicatie run op heroku:
+    database_URL = environ['DATABASE_URL']
+    database_URL = database_URL.replace("postgres://", "postgresql://")
+    engine = create_engine(database_URL)
+else:
+    # web applicatie op mijn eigen computer:
+    engine = create_engine("postgresql://petlover:petlover@localhost/catdog")
 
 def cat_voted():
     with engine.begin() as connection:
